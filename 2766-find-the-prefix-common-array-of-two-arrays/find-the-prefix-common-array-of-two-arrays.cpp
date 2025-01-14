bab5f8dd9cfd5df5b251d4b94da858a2f9b2b5a6
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-14 08:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (58.10%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -4,15 +4,25 @@ public:
         vector<int> result;
 
         int n = A.size();
-        set<int> a,b;
-        
+        vector<int> s(n + 1, 0);
+        unordered_set<int> a, b;
+        int count = 0;
         for (int i = 0; i < n; i++) {
-            int count = 0;
-            //Two set Approach:
-            a.insert(A[i]); b.insert(B[i]);
-           for(auto&i: a){
-                if(b.count(i)) count++;
-           }
+
+            //     //Two set Approach:
+            //     a.insert(A[i]); b.insert(B[i]);
+            //    for(auto&i: a){
+            //         if(b.count(i)) count++;
+            //    }
+
+            // Using frequency array approach:
+            s[A[i]]++; s[B[i]]++;
+            if(A[i]!=B[i])
+            {if(s[A[i]]==2) count++;
+            if(s[B[i]]==2) count++;}
+            else {
+                if(s[A[i]]==2) count++;
+            }
 
             result.push_back(count);
         }
