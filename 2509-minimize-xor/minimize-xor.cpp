7c563f6354d4a41564ce1300ce10f182b626e8cc
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-15 08:27:56 +0530

    Time: 726 ms (2.34%) | Memory: 7.8 MB (91.12%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    bool isSet(int n, int a){
-        return (n&(1<<a) ? true : false);
-    }
+    bool isSet(int n, int a) { return (n & (1 << a) ? true : false); }
 
-    void set(int &a, int i){
-        a = a|(1<<i);
-    }
+    void set(int& a, int i) { a = a | (1 << i); }
 
-    void unset(int &a, int t){
-        a = a & ~(1<<t);
-    }
+    void unset(int& a, int t) { a = a & ~(1 << t); }
 
     int minimizeXor(int num1, int num2) {
-        int r = num1;
-        int curr = 0;
-        int targetbit = __builtin_popcount(num2);
-        int result = __builtin_popcount(num1);
-        while(result<targetbit){
-            if(!isSet(r,curr)){
-                set(r,curr);
-                result++;
+        // int r = num1;
+        // int curr = 0;
+        // int targetbit = __builtin_popcount(num2);
+        // int result = __builtin_popcount(num1);
+        // while(result<targetbit){
+        //     if(!isSet(r,curr)){
+        //         set(r,curr);
+        //         result++;
+        //     }
+        //     curr++;
+        // }
+
+        // curr = 0;
+        // while(result>targetbit){
+        //     if(isSet(r,curr)){
+        //         unset(r,curr);
+        //         result--;
+        //     }
+        //     curr++;
+        // }
+
+        int p = __builtin_popcount(num2);
+        int t = num1;
+        while (true) {
+            if (__builtin_popcount(t) == p) {
+                return t;
             }
-            curr++;
+            else if(__builtin_popcount(t) > p){
+                t--;
+            }
+           else t++;
         }
 
-        curr = 0;
-        while(result>targetbit){
-            if(isSet(r,curr)){
-                unset(r,curr);
-                result--;
-            }
-            curr++;
-        }
-
-        return r;
+        return t;
     }
 };
