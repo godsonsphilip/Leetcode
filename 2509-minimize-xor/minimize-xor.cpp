d062a28a11106a21b206bc25c68b75e1f09c0a92
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-15 08:22:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (40.19%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isSet(int n, int a){
+        return (n&(1<<a) ? true : false);
+    }
+
+    void set(int &a, int i){
+        a = a|(1<<i);
+    }
+
+    void unset(int &a, int t){
+        a = a & ~(1<<t);
+    }
+
+    int minimizeXor(int num1, int num2) {
+        int r = num1;
+        int curr = 0;
+        int targetbit = __builtin_popcount(num2);
+        int result = __builtin_popcount(num1);
+        while(result<targetbit){
+            if(!isSet(r,curr)){
+                set(r,curr);
+                result++;
+            }
+            curr++;
+        }
+
+        curr = 0;
+        while(result>targetbit){
+            if(isSet(r,curr)){
+                unset(r,curr);
+                result--;
+            }
+            curr++;
+        }
+
+        return r;
+    }
+};
