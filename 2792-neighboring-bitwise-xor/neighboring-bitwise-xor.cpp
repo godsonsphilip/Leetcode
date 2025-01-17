3d57c79816eb17e813efe1f470cffc9b599102db
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-17 06:45:37 +0530

    Time: 34 ms (5.61%) | Memory: 290 MB (5.61%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        //Parity of 1's count: O(n) S.C: O(1)
-        // int c = count(derived.begin(), derived.end(), 1);
-        // return ((c&1) ? false : true);
-
-        //Cummulative XOR: O(n) S.C.: O(1)
-        int result = derived[0];
-        for(int i = 1; i<derived.size(); i++){
-            result^= derived[i];
+        // Brute Force Approach:
+        int n = derived.size();
+        vector<int> a1;
+        // case-1: a1 starts with 1
+        a1 = {1};
+        for (int i = 1; i < n; i++) {
+            a1.push_back(derived[i - 1] ^ a1[i - 1]);
         }
-         return (result==0) ? true : false;
+        int k = 0;
+        if (derived[n - 1] == a1.back()^a1.front()) {
+           
+                k = 1;
+        } 
+        if(k) return true;
+
+        
+        a1 = {0};
+
+        
+        for (int i = 1; i < n; i++) {
+            a1.push_back(derived[i - 1] ^ a1[i - 1]);
+        }
+         k = 0;
+        if (derived[n - 1] == a1.front()^a1.back()) {
+            k=1;
+        }
+        
+            
+        if(k) return true;
+        return false;
+        // Parity of 1's count: O(n) S.C: O(1)
+        //  int c = count(derived.begin(), derived.end(), 1);
+        //  return ((c&1) ? false : true);
+
+        // Cummulative XOR: O(n) S.C.: O(1)
+        //  int result = derived[0];
+        //  for(int i = 1; i<derived.size(); i++){
+        //      result^= derived[i];
+        //  }
+        //   return (result==0) ? true : false;
     }
 };
