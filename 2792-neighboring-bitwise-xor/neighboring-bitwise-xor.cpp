42bdd134dfb7de287459d53f76e1e39f746db6a2
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-17 06:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 263.9 MB (74.77%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        //Parity of 1's count:
-        int c = count(derived.begin(), derived.end(), 1);
-        return ((c&1) ? false : true);
+        //Parity of 1's count: O(n) S.C: O(1)
+        // int c = count(derived.begin(), derived.end(), 1);
+        // return ((c&1) ? false : true);
+
+        //Cummulative XOR: O(n) S.C.: O(1)
+        int result = derived[0];
+        for(int i = 1; i<derived.size(); i++){
+            result^= derived[i];
+        }
+         return (result==0) ? true : false;
     }
 };
