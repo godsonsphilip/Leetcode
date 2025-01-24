c91a7f0556918ecf0a62e275d36463d7860f35a3
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-24 08:59:46 +0530

    Time: 27 ms (64.96%) | Memory: 58.6 MB (62.77%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool dfs(int i, vector<vector<int>>& graph, unordered_map<int, bool>& myMap) {
+        if (myMap.find(i) != myMap.end()) {
+            return myMap[i];
+        }
+        myMap[i] = false; // Mark as unsafe initially
+        for (int j = 0; j < graph[i].size(); j++) {
+            if (!dfs(graph[i][j], graph, myMap)) {
+                return myMap[i];
+            }
+        }
+        myMap[i] = true; // Mark as safe
+        return myMap[i];
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> result;
+        unordered_map<int, bool> myMap;
+
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, graph, myMap)) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
