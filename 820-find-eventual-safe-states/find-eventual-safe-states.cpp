824e9d727a45501e7a97448cfc5eb6c0837923c9
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-24 09:01:19 +0530

    Time: 33 ms (64.21%) | Memory: 58.4 MB (62.92%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    bool dfs(int i, vector<vector<int>>& graph, unordered_map<int, bool>& myMap) {
+    bool dfs(int i, vector<vector<int>>& graph,
+             unordered_map<int, bool>& myMap) {
         if (myMap.find(i) != myMap.end()) {
             return myMap[i];
         }
-        myMap[i] = false; // Mark as unsafe initially
+        myMap[i] = false;
         for (int j = 0; j < graph[i].size(); j++) {
-            if (!dfs(graph[i][j], graph, myMap)) {
-                return myMap[i];
-            }
+            if (!dfs(graph[i][j], graph, myMap)){
+                    return myMap[i];
         }
-        myMap[i] = true; // Mark as safe
-        return myMap[i];
     }
+    myMap[i] = true;
+    return myMap[i];
+}
 
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> result;
-        unordered_map<int, bool> myMap;
 
-        for (int i = 0; i < n; i++) {
-            if (dfs(i, graph, myMap)) {
-                result.push_back(i);
-            }
+vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    int n = graph.size();
+    vector<int> result;
+    unordered_map<int, bool> myMap;
+
+    for (int i = 0; i < n; i++) {
+        if (dfs(i, graph, myMap)) {
+            result.push_back(i);
         }
-        return result;
     }
-};
+    return result;
+}
+}
+;
