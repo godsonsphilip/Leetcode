193fa3e1770fa7da386ae0cdc1299a595405df43
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-01-29 22:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (31.57%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++ ){
+            for(int j =1; j<nums.size();j++ ){
+                if(nums[j]<nums[j-1]){
+                    swap(nums[j],nums[j-1]);
+                }
+            }
+        }
+    }
+};
