c866c5671089d379bb9a93a504056ce7c4912205
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-02-03 19:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 28 MB (27.06%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -3,40 +3,30 @@ public:
     int longestMonotonicSubarray(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return 1;
-        int count1 = 0, count2 = 0;
-        int l = 0, r = 1;
+        int increasing = 1, decreasing = 1;
 
-        int c = 1;
-        //For Decreasing Function:
-        while(l<n && r<n){
-            if(nums[l]>nums[r]){
-                r++;
-                l++;
-                c++;
+        //For maintaining the global maximum:
+        int result = 0;
+
+        for(int i = 0; i<n-1; i++){
+            //Increasing Function:
+            if(nums[i]<nums[i+1]){
+                increasing++;
+                decreasing = 1;
             }
+
+            else if(nums[i]>nums[i+1]){
+                decreasing++;
+                increasing = 1;
+            }
+
             else {
-                l = r;
-                r++;
-                c = 1;
+                increasing = 1; decreasing = 1;
             }
-            count2 = max(count2, c);
-        }
 
-        l = 0; r = 1; c= 1;
-        //For increasing function:
-        while(l<n and r<n){
-            if(nums[l]<nums[r]) {
-                r++; c++; l++;
-                
-            }
-            else{
-                l = r;
-                r++;
-                c = 1;
-            }
-            count2 = max(count2, c);
+            result = max(result, max(increasing, decreasing));
         }
-
-        return max(count1, count2);
+       
+        return result;
     }
 };
