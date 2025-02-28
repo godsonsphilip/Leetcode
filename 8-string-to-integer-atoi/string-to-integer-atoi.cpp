58a22b93abda2004b075283a8f994a68d27deb5c
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-02-28 08:37:05 +0530

    Time: 1 ms (25.30%) | Memory: 10.2 MB (5.27%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    int myAtoi(string s) {
+    int myAt(string s, int t, int sign , long ans) {
         int n = s.length();
-        int t = 0;
-        long ans = 0;
-        bool key = true;
-        int sign = 1;
-        // Just to remove the leading white spaces:
-        while (t < n && s[t] == ' ') {
-            t++;
-        }
 
-        //Checking thr signs:
-        if (s[t] == '-' || s[t]=='+') {
-            sign = (s[t]=='-') ? -1 : 1;
-            t++; 
-        }
+        if (t == 0) {
+            // Just to remove the leading white spaces:
+            while (t < n && s[t] == ' ') {
+                t++;
+            }
 
-        // Skipping the leading zeroes:
-        while (t < n and s[t] == '0') {
-            t++;
+            // Checking thr signs:
+            if (s[t] == '-' || s[t] == '+') {
+                sign = (s[t] == '-') ? -1 : 1;
+                t++;
+            }
+
+            // Skipping the leading zeroes:
+            while (t < n and s[t] == '0') {
+                t++;
+            }
         }
 
         // Checking whether the character is a non-integer or not?
-        while (t < n and isdigit(s[t])) {
-            ans = ans * 10 + (s[t] - '0');
-            if (ans * sign >= INT_MAX)
-                return INT_MAX;
-            if (ans * sign <= INT_MIN)
-                return INT_MIN;
 
-            t++;
-        }
+        if ( t >= n or !isdigit(s[t]))
+            return ans * sign;
+        ans = ans * 10 + (s[t]-'0');
+        t++;
+        if (ans * sign >= INT_MAX)
+            return INT_MAX;
+        if (ans * sign <= INT_MIN)
+            return INT_MIN;
+        return myAt(s, t, sign, ans);
+    }
 
-        return ans * sign;
+    int myAtoi(string s){
+        return myAt(s, 0, 1, 0);
     }
 };
