80c05cde058ce43386dc5693aeb18888bbe94a52
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-03-02 12:29:57 +0530

    Time: 4 ms (19.09%) | Memory: 15 MB (34.31%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
+                                    vector<vector<int>>& nums2) {
         vector<vector<int>> result;
-        //First comparing with the frist matrix:
-        for(int i = 0; i<nums1.size(); i++){
-        bool key = false;
-            for(int j = 0; j<nums2.size(); j++){
-                if(nums1[i][0]==nums2[j][0]){
-                    result.push_back({nums1[i][0], (nums1[i][1]+nums2[j][1])});
-                    key = true;
-                    break;
-                }
-            }
-            if(!key){
-                result.push_back({nums1[i][0], nums1[i][1]});
-            }
+        unordered_map<int, int> myMap;
+        for (auto& i : nums1) {
+            myMap[i[0]] += i[1];
         }
-
-        for(int i = 0; i<nums2.size(); i++){
-        bool k = true; 
-            for(int j = 0; j<nums1.size(); j++){
-                if(nums1[j][0]== nums2[i][0]){
-                    k = false;
-                    break;
-                }
-            }
-            if(k){
-                result.push_back({nums2[i][0], nums2[i][1]});
-            }
+        for (auto& i : nums2) {
+            myMap[i[0]] += i[1];
         }
-        
+        for (auto& i : myMap) {
+            int a = i.first;
+            int b = i.second;
+            result.push_back({a, b});
+        }
         sort(result.begin(), result.end());
         return result;
-        
     }
 };
