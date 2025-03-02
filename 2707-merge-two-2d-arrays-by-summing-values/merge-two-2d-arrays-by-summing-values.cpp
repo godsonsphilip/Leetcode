05a79d5d611bd5cf287c9d446c82ab6c342dd98a
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-03-02 12:33:12 +0530

    Time: 1 ms (45.39%) | Memory: 15.1 MB (28.71%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
                                     vector<vector<int>>& nums2) {
         vector<vector<int>> result;
-        unordered_map<int, int> myMap;
+        map<int, int> myMap;
         for (auto& i : nums1) {
             myMap[i[0]] += i[1];
         }
@@ -15,7 +15,7 @@ public:
             int b = i.second;
             result.push_back({a, b});
         }
-        sort(result.begin(), result.end());
+       
         return result;
     }
 };
