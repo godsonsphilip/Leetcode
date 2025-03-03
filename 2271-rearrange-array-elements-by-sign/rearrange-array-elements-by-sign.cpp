3dae77346af58cdef2040aa1ec1fc4f1592ab56d
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-03-03 17:08:27 +0530

    Time: 7 ms (60.69%) | Memory: 134.2 MB (37.89%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -3,23 +3,46 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> result;
         int n = nums.size();
-        vector<int> t1, t2;  //positive, negative
+        
+        int l = 0, r = 0;  //Positive and negative
 
-        //Given the fact, that neither of the two arrays, i.e., t1 and t2 are going tobe empty.
-        //First fill in the positive number then the negative number and so on.....
-        for(auto& i: nums){
-            if(i>0){
-                t1.push_back(i);
+        while(l<n and r<n){
+            while(nums[l]<0){
+                l++;
             }
-            else{
-                t2.push_back(i);
+
+            result.push_back(nums[l]);
+            l++;
+
+            while(nums[r]>0){
+                r++;
             }
+
+            result.push_back(nums[r]);
+            r++;
         }
 
-        for(int i = 0; i<(n/2); i++){
-            result.push_back(t1[i]);
-            result.push_back(t2[i]);
-        }
+        
+        
+        
+        // vector<int> t1, t2;  //positive, negative
+
+        // //Given the fact, that neither of the two arrays, i.e., t1 and t2 are going tobe empty.
+        // //First fill in the positive number then the negative number and so on.....
+        // for(auto& i: nums){
+        //     if(i>0){
+        //         t1.push_back(i);
+        //     }
+        //     else{
+        //         t2.push_back(i);
+        //     }
+        // }
+
+        // for(int i = 0; i<(n/2); i++){
+        //     result.push_back(t1[i]);
+        //     result.push_back(t2[i]);
+        // }
+        
         return result;
     }
 };
