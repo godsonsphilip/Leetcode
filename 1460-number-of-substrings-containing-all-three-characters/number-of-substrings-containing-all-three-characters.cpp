4573cdb6afce36e03b0f13422599b7aed1976eef
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-03-11 11:55:13 +0530

    Time: 4 ms (82.48%) | Memory: 11 MB (57.30%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+       int count = 0;
+       int a = 0, b = 0, c = 0;
+       int n = s.size();
+       int left = 0;
+       for(int right = 0; right<n; right++){
+        if(s[right]=='a') a++;
+        else if(s[right]=='b') b++;
+        else if(s[right]=='c') c++;
+
+        while(a>0 && b>0 && c>0){
+            count += n-right; //This counts the number of substrings satisfying condition which starts at left and ends at right:
+
+            if(s[left]=='a')   a--;
+            else if(s[left]=='b') b--;
+            else if(s[left]=='c') c--;
+            left++;
+        }   
+       }
+       return count;
+    }
+};
