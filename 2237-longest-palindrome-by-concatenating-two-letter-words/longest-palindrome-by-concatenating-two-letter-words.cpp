4df80696e7d5e6c764d5cdd0b6efd63dfbe08cca
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-05-25 15:37:04 +0530

    Time: 131 ms (27.37%) | Memory: 171.7 MB (98.77%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -6,28 +6,32 @@ public:
         int flag = 0;
 
         unordered_map<string, int> myMap;
-        for (auto& i : words) {
+        for(auto& i: words){
             myMap[i]++;
         }
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i<n; i++){
             string t = words[i];
             reverse(t.begin(), t.end());
-
-            if (t == words[i]) {
-                if (myMap[t] == 1 && flag == 0) {
+            if(t==words[i]){
+                
+                if(myMap[t]==1 && flag==0){
                     flag = 1;
                     maxLen += 2;
                     myMap[t]--;
-                } else if (myMap[t] >= 2) {
+                }
+
+               else if (myMap[t] >= 2) {
                     maxLen += 4;
                     myMap[t] -= 2;
                 }
-            } else if (myMap[t] > 0 && myMap[words[i]] > 0) {
+            }
+           else if (myMap[t] > 0 && myMap[words[i]] > 0) {
                 maxLen += 4;
                 myMap[t]--;
                 myMap[words[i]]--;
             }
+
         }
 
         return maxLen;
