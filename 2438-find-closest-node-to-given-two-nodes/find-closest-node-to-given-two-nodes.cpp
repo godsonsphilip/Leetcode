05728262e89207ec2d4a413f84c7055eabf41a49
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-05-30 10:18:42 +0530

    Time: 25 ms (40.67%) | Memory: 123.7 MB (27.83%) - LeetSync

---
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int V = edges.size();
+        vector<int> dist1(V,INT_MAX), dist2(V, INT_MAX);
+        bfs(edges, node1, dist1);
+        bfs(edges, node2, dist2);
+
+        vector<int> temp(V, 0);
+        for(int i = 0; i<V; i++){
+            temp[i] = max(dist1[i], dist2[i]);
+        }
+
+        int result = -1, mini = INT_MAX;
+        for(int i = 0; i<V; i++){
+            if(mini>temp[i]){
+                mini = temp[i];
+                result = i;
+            }
+        }
+        return result;
+    }
+
+    void bfs(vector<int> edges, int src, vector<int>& dist) {
+        int n = edges.size() + 1;
+        vector<int> vis(n+1, 0);
+        queue<int> q;
+        q.push(src);
+        dist[src] = 0;
+        int distance = 0;
+
+        while(!q.empty()){
+            int val = q.front();
+            q.pop();
+            if(vis[val]) continue;
+            vis[val] = 1;
+            int neighbour = edges[val];
+            if(neighbour != -1 and vis[neighbour]==0){
+                q.push(neighbour);
+                distance++;
+                dist[neighbour] = distance;
+            }
+        }
+    }
+};
