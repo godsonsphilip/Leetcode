351f6cf250f8e41507b7ac4e93ee1027ca1ad2af
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-06-01 11:25:59 +0530

    Time: 35 ms (12.94%) | Memory: 8.9 MB (84.71%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -14,16 +14,29 @@ public:
     //     return ans;
     // }
 
-    long long distributeCandies(int n, int limit) { 
-        return cal(n+2) -3*cal(n-(limit+1)+2) + 3*cal(n-2*(limit+1)+2) - cal(n-3*(limit+1)+2);
-     }
+    // Principle of INclusion and Exclusion: t.c: O(1);
+    // long long distributeCandies(int n, int limit) { 
+    //     return cal(n+2) -3*cal(n-(limit+1)+2) + 3*cal(n-2*(limit+1)+2) - cal(n-3*(limit+1)+2);
+    //  }
 
-     long long cal(int n){
-        if(n<=1) return 0;
-        return (long long)n*(n-1)/2;
-     }
+    //  long long cal(int n){
+    //     if(n<=1) return 0;
+    //     return (long long)n*(n-1)/2;
+    //  }
 
+    // T.C = O(n);
+    long long distributeCandies(int n, int limit){
+        long long ans = 0;
+        for(int i  = 0; i<= min(n,limit); i++){
+            int s = n-i;
+            if(s>2*limit) continue;
+            int jmax = min(limit, s);
+            int jmin = max(0, s-limit);
 
-    // Principle of INclusion and Exclusion:
+            ans += (long long)(jmax-jmin + 1);
+        }
+        return ans;
+
+    }
 
 };
