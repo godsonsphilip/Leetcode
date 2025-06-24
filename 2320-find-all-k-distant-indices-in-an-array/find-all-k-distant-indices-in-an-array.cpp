80e36bb96b1008fec7006061ac66536062af111a
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-06-24 21:54:48 +0530

    Time: 11 ms (44.95%) | Memory: 14.8 MB (55.05%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         vector<int> result;
         vector<int> vis(n,0);
-        vector<int> temp;
+        
         for(int i = 0; i<n; i++){
             if(nums[i]==key){
-                for(int j = 0; j<n; j++){
-                    if(abs(i-j)<=k and vis[j]==0){
+                for(int j = max(0,i-k); j<min(i+k+1,n); j++){
+                    if(vis[j]==0){
                         result.push_back(j);
                         vis[j] = 1;
                     }
