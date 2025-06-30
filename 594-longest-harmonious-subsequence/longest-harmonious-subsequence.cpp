ff9b0045ba7874633d93d97727896df602f5a7e2
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-06-30 09:56:11 +0530

    Time: 26 ms (48.91%) | Memory: 43.8 MB (59.83%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -3,13 +3,13 @@ public:
     int findLHS(vector<int>& nums) {
         int n = nums.size();
         int maxi = 0;
-        if(!is_sorted(nums.begin(), nums.end())) sort(nums.begin(), nums.end());
+       
         unordered_map<int, int> myMap;
-        for(int i = 0; i<n; i++) myMap[nums[i]] = i;
-        for(int i = 0; i<n; i++){
-            int val = nums[i];
-            if(find(nums.begin(), nums.end(), val+1)==nums.end()) continue;
-            maxi = max(maxi, (myMap[val+1]-i+1));
+        for(auto& i: nums) myMap[i]++;
+        for(auto& [key, val]: myMap){
+            if(myMap.count(key+1)!=0){
+                maxi = max(maxi, val + myMap[key+1]);
+            }
         }
         
         return maxi;
