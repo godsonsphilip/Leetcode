79860ec43d830e45b80bb3593a55a2865acd7902
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-07-07 18:01:56 +0530

    Time: 1 ms (47.61%) | Memory: 10 MB (58.19%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,9 +4,9 @@ public:
         int n = s.size();
         int i = 0, j = n-1;
         while(i<j){
-            while(i<j and !isalnum(s[i])) i++;
+            while(i<j and (!isalpha(s[i]) and !isdigit(s[i]))) i++;
             if(isupper(s[i])) s[i] = tolower(s[i]);
-            while(j>i and !isalnum(s[j])) j--;
+            while(j>i and (!isalpha(s[j]) and !isdigit(s[j]))) j--;
             if(isupper(s[j])) s[j] = tolower(s[j]);
             if(s[i]!=s[j]) return false;
             i++;
