1f9c8d90fe1ab2c03f693bc6f4ce1547e290fcbf
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-07-25 18:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.49%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i=0; i<nums.size(); i++ ){
-            for(int j =1; j<nums.size();j++ ){
-                if(nums[j]<nums[j-1]){
-                    swap(nums[j],nums[j-1]);
-                }
-            }
+        //Slightly better Approach: T.C. O(2*n);
+        int c0 = 0, c1 = 0, c2 = 0;
+        for(int i: nums) {
+            if(i==0) c0++;
+            else if(i==1) c1++;
+            else c2++;
         }
+
+        for(int i = 0; i<c0; i++){
+            nums[i] = 0;
+        }
+        for(int i = 0; i<c1; i++) nums[i + c0] = 1;
+        for(int i = c0+c1; i<nums.size(); i++) nums[i] = 2;
     }
 };
