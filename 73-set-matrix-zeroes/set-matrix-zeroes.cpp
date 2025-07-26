b973bf2ced721c0266c6721c48758e7a5d7923ce
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-07-27 04:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (79.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -26,23 +26,63 @@ public:
         //     }
         // } 
 
-        //Using row and Column arrays:
+        //Using row and Column arrays:   S.C. : O(2*n);
         int m = nums.size(), n = nums[0].size();
-        vector<int> rows(m, 0), cols(n, 0);
+        // vector<int> rows(m, 0), cols(n, 0);
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(nums[i][j]==0){
+        //             rows[i]=1; cols[j]=1;
+        //         }
+        //     }
+        // }
+
+        // for(int i = 0; i<m; i++){
+        //     for(int j = 0; j<n; j++){
+        //         if(rows[i] || cols[j]){
+        //             nums[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+
+        //Optimal Approach: S.C. : O(1);
+        int col0 = 1;
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
+                if(j==0){
+                    if(nums[i][j]==0){
+                        col0 = 0;
+                        continue;
+                    }
+                }
                 if(nums[i][j]==0){
-                    rows[i]=1; cols[j]=1;
+                    nums[0][j]=0;
+                    nums[i][0] = 0;
                 }
             }
         }
 
-        for(int i = 0; i<m; i++){
-            for(int j = 0; j<n; j++){
-                if(rows[i] || cols[j]){
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                if(!nums[0][j] || !nums[i][0]){
                     nums[i][j] = 0;
                 }
             }
         }
+
+        for(int j = 1; j<n; j++){
+            if(!nums[0][0]){
+                nums[0][j]=0;
+            }
+        }
+       
+
+        for(int i = 0; i<m; i++){
+            if(!col0){
+                nums[i][0] = 0;
+            }
+        }
+
     }
 };
