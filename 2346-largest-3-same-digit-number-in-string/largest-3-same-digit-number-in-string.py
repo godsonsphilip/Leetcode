ce0b05897a8dc88ced12ae4190c452d59fa255b6
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-08-14 12:39:31 +0530

    Time: 3 ms (67.70%) | Memory: 17.6 MB (87.42%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -1,24 +1,10 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        mpp = set()
         n = len(num)
         result = ''
-        left, right = 0, 0
-        
-        while right < n:
-            # Check for a block of same digits
-            left = right
-            while right < n and num[right] == num[left]:
-                right += 1
-            
-            # If length of block >= 3, store digit
-            if right - left >= 3:
-                mpp.add(num[left])
-        
-        # If no triple found, return empty string
-        if not mpp:
-            return ''
-        
-        # Get largest digit and form result
-        a = max(mpp)
-        return a * 3
+        for i in range(0, n-2):
+            if num[i]==num[i+1]==num[i+2]:
+                cand = num[i:i+3]
+                if cand>result:
+                    result = cand
+        return result
