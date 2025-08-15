925b8d6ccfaf20f10d34587e4c898ce2fe545cee
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-08-15 05:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.20%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -1,9 +1,12 @@
-class Solution:
-    def isPowerOfFour(self, n: int) -> bool:
-        if n<1: return False
-        while n!= 1:
-            rem = n%4
-            if rem :
-                return False
-            n //= 4
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        if n<=0: return False
+        while n>1:
+            if n%4!=0: return False
+            n /= 4
         return True
