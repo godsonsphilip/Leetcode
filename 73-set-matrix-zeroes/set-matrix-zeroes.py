557600db62eb8ede19dcf6af1b0aafcf2cdb6949
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-08-15 05:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (73.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,18 +1,32 @@
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+class Solution(object):
+    def setZeroes(self, matrix):
         """
-        Do not return anything, modify matrix in-place instead.
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        n1, n2 = len(matrix), len(matrix[0])
-        row = [1]*n2
-        col = [1]*n1
-        for i in range(0, n1):
-            for j in range(0, n2):
-                if matrix[i][j]==0:
-                    row[j] = 0
-                    col[i] = 0
         
-        for i in range(0, n1):
-            for j in range(0, n2):
-                if(not row[j] or not col[i]):
+        m = len(matrix)
+        n = len(matrix[0])
+        row0 = 1
+        for i in range(0, m):
+            for j in range(0, n):
+                if i==0:
+                    if matrix[0][j]==0:
+                        row0 = 0
+                        continue
+
+                if matrix[i][j]==0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+        
+        for i in range(1, m):
+            for j in range(1,n):
+                if matrix[0][j]==0 or matrix[i][0]==0:
                     matrix[i][j] = 0
+        if matrix[0][0]==0:
+            for i in range(1,m):
+                matrix[i][0] = 0
+        
+        if row0 == 0:
+            for i in range(0, n):
+                matrix[0][i] = 0
