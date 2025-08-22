1313996e40f793f6c24e8dde4e4628dab6e11c05
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-08-22 18:19:50 +0530

    Time: 6 ms (47.72%) | Memory: 17.8 MB (44.51%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -3,7 +3,7 @@ class Solution:
         myMap = {'M':1000,'CM':900, 'D':500,'CD':400, 'C':100,'XC':90, 'L':50,'XL':40, 'X':10, 'IX':9 ,'V':5, 'IV': 4,'I':1}
         result = ""
         for key, val in myMap.items():
-            if num==0: return result
+            
             temp = num//val
             result += key * temp
             num %= val
