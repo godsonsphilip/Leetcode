b88eb7ffcef526b8280aedf3a18edd9f431d43b9
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-08-23 06:22:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.86%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -3,11 +3,12 @@ public:
     void backtrack(vector<int>& nums, int index, vector<int>& ds, vector<vector<int>>& result, int count, int sum){
         if(sum==0 and ds.size()==count) {result.push_back(ds); return;}
 
-        if(index>=nums.size() or nums[index]>sum or ds.size()>count) return;
-        
+        if(index>=nums.size() or nums[index]>sum) return;
+
+        if(ds.size()<count){
         ds.push_back(nums[index]);
         backtrack(nums, index+1, ds, result, count, sum-nums[index]);
-        ds.pop_back();
+        ds.pop_back();}
         backtrack(nums, index+1, ds, result, count, sum);
     }
 
