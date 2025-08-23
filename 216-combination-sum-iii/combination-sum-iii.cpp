7089817dc9d55c70bd6f715aff9a41642deb5b0d
Godson S. Philip <148892862+godsonsphilip@users.noreply.github.com>
2025-08-23 06:22:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (80.23%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, int index, vector<int>& ds, vector<vector<int>>& result, int count, int sum){
+        if(sum==0 and ds.size()==count) {result.push_back(ds); return;}
+
+        if(index>=nums.size() or nums[index]>sum or ds.size()>count) return;
+        
+        ds.push_back(nums[index]);
+        backtrack(nums, index+1, ds, result, count, sum-nums[index]);
+        ds.pop_back();
+        backtrack(nums, index+1, ds, result, count, sum);
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        backtrack(nums, 0, temp, result, k, n);
+        return result;
+    }
+};
